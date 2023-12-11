112f6821c7b7f28f37885b80d2ca8d5cf8fdb285
Ayush <80757890+ayush645@users.noreply.github.com>
2023-12-11 15:22:40 +0530

    final change

--- client/src/redux/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const BOOKSSHOW = "BOOKSSHOW";
-export const CARTITEMS = "CARTITEMS";
-export const SEARCHITEM = "SEARCHITEM";
-export const PRICE = "PRICE";
--- client/src/redux/store.js
+++
@@ -1,3 +0,0 @@
-import { Reducer } from "./reducer";
-import { createStore } from "redux";
-export const store = createStore(Reducer);
